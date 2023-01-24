c356ce811fe61267c3dff80b7f766f873ea3f8d2
superminkfan <zikunov_semen@mail.ru>
2023-01-25 09:56:49 +1000

    Шифруем и дешифруем с солью

---
+++ src/main/java/com/AesEncrypt/Aes256Class.java
@@ -0,0 +1,39 @@
+package com.AesEncrypt;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+
+public class Aes256Class {
+
+    private SecretKey secretKey;
+
+    public Aes256Class() {
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+            keyGenerator.init(256);
+            this.secretKey = keyGenerator.generateKey();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+    public byte[] makeAes(byte[] rawMessage, int cipherMode){
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(cipherMode, this.secretKey);
+            byte [] output = cipher.doFinal(rawMessage);
+            if(cipherMode == Cipher.DECRYPT_MODE){
+                byte[]result = new byte[output.length-8];
+                //Выбрасываем последние 8 байт
+                System.arraycopy(output, 0, result, 0, output.length-8);
+                return result;
+            }
+            return output;
+        } catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,8 +1,35 @@
 package com;
 
+import com.AesEncrypt.Aes256Class;
+
+import javax.crypto.Cipher;
+import java.security.SecureRandom;
+
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("ddd");
+    public static void main(String[] args)  {
+        Aes256Class aes256 = new Aes256Class();
+        //Массив для соли
+        byte[] salt = new byte[8];
+        //Исходное сообщение
+        String mes = "Опасность";
+        //Выполним операцию 10 раз
+        for (int i = 0; i < 10; i++) {
+            //Генерация соли
+            SecureRandom random = new SecureRandom();
+            random.nextBytes(salt);
+            //Преобразуем исходный текст в поток байт и добавим полученную соль
+            byte[]srcMessage = mes.getBytes();
+            byte[]fullsrcMessage = new byte[srcMessage.length+8];
+            System.arraycopy(srcMessage, 0, fullsrcMessage, 0, srcMessage.length);
+            System.arraycopy(salt, 0, fullsrcMessage, srcMessage.length, salt.length);
+            //Шифруем
+            byte[] shifr = aes256.makeAes(fullsrcMessage, Cipher.ENCRYPT_MODE);
+            System.out.println(new String(shifr));
+            //Дешифруем
+            byte[] src = aes256.makeAes(shifr, Cipher.DECRYPT_MODE);
+            System.out.println(new String(src));
+        }
+
     }
 }
