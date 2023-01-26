f0dfc601c255f517a8bff3a25e24a76ca9db6467
superminkfan <zikunov_semen@mail.ru>
2023-01-26 12:51:12 +1000

    log4j2 init

---
+++ src/main/java/com/WebStuff/SpringWTFApplication.java
@@ -0,0 +1,31 @@
+package com.WebStuff;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class SpringWTFApplication
+{
+
+   private static final Logger log = LogManager.getLogger(SpringWTFApplication.class);
+
+    public static void main(String[] args) {
+
+        log.debug("Debugging log");
+        log.info("Info log");
+        log.warn("Hey, This is a warning!");
+        log.error("Oops! We have an Error. OK");
+        log.fatal("Damn! Fatal error. Please fix me.");
+        SpringApplication.run(SpringWTFApplication.class, args);
+    }
+
+
+
+
+
+}
