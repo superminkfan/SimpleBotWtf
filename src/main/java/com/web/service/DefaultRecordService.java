bd033bf3ec2867005724b751958d9a9e2adc67b6
superminkfan <zikunov_semen@mail.ru>
2023-01-26 22:19:40 +1000

    ref1

--- src/main/java/com/web/service/DefaultRecordService.java
+++ src/main/java/com/web/service/DefaultRecordService.java
@@ -12,50 +12,51 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-@Service
-@RequiredArgsConstructor
-public class DefaultRecordService implements RecordService {
-    private final RecordRepository recordRepository;
-    private final RecordToEntityMapper mapper;
+//@Service
+//@RequiredArgsConstructor
+public class DefaultRecordService  {
+//public class DefaultRecordService implements RecordService {
+//    private final RecordRepository recordRepository;
+//    private final RecordToEntityMapper mapper;
+//
+//    @Override
+//    public Record getRecordById(Long id) {
+//        RecordEntity RecordEntity = recordRepository
+//                .findById(id)
+//                .orElseThrow(() -> new RecordNotFoundException("Record not found: id = " + id));
+//
+//        return mapper.recordEntityToBook(RecordEntity);
+//    }
+//
+//    @Override
+//    public List<Record> getAllRecords() {
+//        Iterable<RecordEntity> iterable = recordRepository.findAll();
+//        return StreamSupport.stream(iterable.spliterator(), false)
+//                .map(mapper::recordEntityToBook)
+//                .collect(Collectors.toList());
+//    }
+//
+//    @Override
+//    public List<Record> findByChatId(String username) {
+//        Iterable<RecordEntity> iterable = recordRepository.findAllByUsernameContaining(username);
+//        return StreamSupport.stream(iterable.spliterator(), false)
+//                .map(mapper::recordEntityToBook)
+//                .collect(Collectors.toList());
+//    }
+//
+//    @Override
+//    public void addRecord(Record record) {
+//        RecordEntity RecordEntity = mapper.recordToBookEntity(record);
+//        recordRepository.save(RecordEntity);
+//    }
+//
+//    @Override
+//    public void editRecord(Record record) {
+//        if (!recordRepository.existsById(record.getId()))
+//            throw new RecordNotFoundException("Record not found: id = " + record.getId());
+//
+//        RecordEntity recordEntity = mapper.recordToBookEntity(record);
+//        recordRepository.save(recordEntity);
 
-    @Override
-    public Record getRecordById(Long id) {
-        RecordEntity RecordEntity = recordRepository
-                .findById(id)
-                .orElseThrow(() -> new RecordNotFoundException("Record not found: id = " + id));
-
-        return mapper.recordEntityToBook(RecordEntity);
-    }
-
-    @Override
-    public List<Record> getAllRecords() {
-        Iterable<RecordEntity> iterable = recordRepository.findAll();
-        return StreamSupport.stream(iterable.spliterator(), false)
-                .map(mapper::recordEntityToBook)
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public List<Record> findByChatId(String username) {
-        Iterable<RecordEntity> iterable = recordRepository.findAllByUsernameContaining(username);
-        return StreamSupport.stream(iterable.spliterator(), false)
-                .map(mapper::recordEntityToBook)
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public void addRecord(Record record) {
-        RecordEntity RecordEntity = mapper.recordToBookEntity(record);
-        recordRepository.save(RecordEntity);
-    }
-
-    @Override
-    public void editRecord(Record record) {
-        if (!recordRepository.existsById(record.getId()))
-            throw new RecordNotFoundException("Record not found: id = " + record.getId());
-
-        RecordEntity recordEntity = mapper.recordToBookEntity(record);
-        recordRepository.save(recordEntity);
-
-    }
+  //  }
 }
